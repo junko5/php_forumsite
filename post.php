e0f019497b72ac9328bcda1c9aaaf2f5277f0bcb
Junko <70782949+junko5@users.noreply.github.com>
2024-06-17 09:45:07 +0900

    Update post.php
    Clean code, adjust indentation, and translate UI to English

--- post.php
+++ post.php
@@ -1,14 +1,13 @@
 <?php
 session_start();
-require('dbconnect.php');
+require 'dbconnect.php';
 
-
-if (isset($_SESSION['id']) && ($_SESSION['time'] + 3600 > time())) {
+if (isset($_SESSION['id']) && $_SESSION['time'] + 3600 > time()) {
     $_SESSION['time'] = time();
 
-    $members=$db->prepare('SELECT * FROM members WHERE id=?');
-    $members->execute(array($_SESSION['id']));
-    $member=$members->fetch();
+    $members = $db->prepare('SELECT * FROM members WHERE id=?');
+    $members->execute([$_SESSION['id']]);
+    $member = $members->fetch();
 } else {
     header('Location: login.php');
     exit();
@@ -16,8 +15,8 @@ if (isset($_SESSION['id']) && ($_SESSION['time'] + 3600 > time())) {
 
 if (!empty($_POST)) {
     if (isset($_POST['token']) && $_POST['token'] === $_SESSION['token']) {
-        $post=$db->prepare('INSERT INTO posts SET created_by=?, post=?, created=NOW()');
-        $post->execute(array($member['id'] , $_POST['post']));
+        $post = $db->prepare('INSERT INTO posts SET created_by=?, post=?, created=NOW()');
+        $post->execute([$member['id'], $_POST['post']]);
         header('Location: post.php');
         exit();
     } else {
@@ -26,8 +25,7 @@ if (!empty($_POST)) {
     }
 }
 
-$posts=$db->query('SELECT m.name, p.* FROM members m  JOIN posts p ON m.id=p.created_by ORDER BY p.created DESC');
-
+$posts = $db->query('SELECT m.name, p.* FROM members m  JOIN posts p ON m.id=p.created_by ORDER BY p.created DESC');
 
 $TOKEN_LENGTH = 16;
 $tokenByte = openssl_random_pseudo_bytes($TOKEN_LENGTH);
@@ -38,52 +36,54 @@ $_SESSION['token'] = $token;
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-	<title>投稿画面</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-	<div class="container">
-		<!-- ★ログアウト★ -->
-		<header>
-		<div class="head">
-		<h1>週末プラン投稿画面</h1>
-		<span class="logout"><a href="login.php">ログアウト</a></span>
+    <div class="container">
 
-		</div>
-		</header>
+        <header>
+            <div class="head">
+                <h1>Weekend Plan Posting Screen</h1>
+                <span class="logout"><a href="login.php">Logout</a></span>
 
-		<form action='' method="post">
-			<input type="hidden" name="token" value="<?=$token?>">
-			<?php if (isset($error['login']) &&  ($error['login'] =='token')): ?>
-				<p class="error">不正なアクセスです。</p>
-			<?php endif; ?>
-			<div class="edit">
-				<p>
-					<?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?>さん、ようこそ
-				</p>
-				<textarea name="post" cols='50' rows='10'><?php echo htmlspecialchars($post??"", ENT_QUOTES); ?></textarea>
-			</div>
+            </div>
+        </header>
 
-			<input type="submit" value="投稿する" class="button02">
-		</form>
+        <form action='' method="post">
+            <input type="hidden" name="token" value="<?= $token ?>">
+            <?php if (isset($error['login']) &&  ($error['login'] =='token')): ?>
+            <p class="error">This is unauthorized access.</p>
+            <?php endif; ?>
+            <div class="edit">
+                <p>
+                    <?php echo htmlspecialchars($member['name'], ENT_QUOTES); ?> san、Welcome
+                </p>
+                <textarea name="post" cols='50' rows='10'><?php echo htmlspecialchars($post ?? '', ENT_QUOTES); ?></textarea>
+            </div>
 
-		<?php foreach($posts as $post): ?>
-		<div class="post" style="margin-top:10px;">
-			<?php echo htmlspecialchars($post['post'], ENT_QUOTES); ?> | 
-			<span class="name">
-				<?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?> | 
-				<?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?> | 
+            <input type="submit" value="投稿する" class="button02">
+        </form>
 
-				<!-- ★削除★ -->
-				<?php if($_SESSION['id'] == $post['created_by']): ?>
-				[<a href="delete.php?id=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">削除</a>]
-				<?php endif; ?>
-				
-			</span>
-		</div>
-		<?php endforeach; ?>
-	</div>
+        <?php foreach($posts as $post): ?>
+        <div class="post" style="margin-top:10px;">
+            <?php echo htmlspecialchars($post['post'], ENT_QUOTES); ?> |
+            <span class="name">
+                <?php echo htmlspecialchars($post['name'], ENT_QUOTES); ?> |
+                <?php echo htmlspecialchars($post['created'], ENT_QUOTES); ?> |
+
+                <?php if($_SESSION['id'] == $post['created_by']): ?>
+                [<a href="delete.php?id=<?php echo htmlspecialchars($post['id'], ENT_QUOTES); ?>">Delete</a>]
+                <?php endif; ?>
+
+            </span>
+        </div>
+        <?php endforeach; ?>
+    </div>
 </body>
+
 </html>
