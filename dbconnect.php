87c5ca47d8c663b33ebbff588925d75891517a54
Junko <70782949+junko5@users.noreply.github.com>
2024-06-17 09:39:23 +0900

    Update dbconnect.php
    Clean code, adjust indentation, and translate UI to English

--- dbconnect.php
+++ dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try {
-    $db = new PDO ('mysql:dbname=forum;host=127.0.0.1; charset=utf8', 'root', '');
+    $db = new PDO('mysql:dbname=forum;host=127.0.0.1; charset=utf8', 'root', '');
 } catch (PDOException $e) {
-    echo 'DB接続エラー' . $e->getMessage();
+    echo 'DB connection error' . $e->getMessage();
 }
 ?>
